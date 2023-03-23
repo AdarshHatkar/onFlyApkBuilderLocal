1392b9017eaf836583503560c62843c71a79388a
Adarsh Hatkar <80952515+AdarshHatkar@users.noreply.github.com>
2023-03-23 15:22:53 +0530

    test

--- mainFun.js
+++ mainFun.js
@@ -1,22 +1,46 @@
+import require from "./require.js";
 import { exec } from "node:child_process";
+import process from "node:process";
+import { fileURLToPath } from 'url';
+import { dirname ,join} from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 // gradle --version
 export let mainFun = () => {
     console.log("hello world");
    
-    exec('gradle --version', (err, stdout, stderr) => {
-        if (err) {
-          console.error(err);
-          return;
-        }
-        console.log(stdout);
-      });
+    // exec('gradle --version', (err, stdout, stderr) => {
+    //     if (err) {
+    //       console.error(err);
+    //       return;
+    //     }
+    //     console.log(stdout);
+    //   });
 
-    exec('cd gaming_app_apk_v103 && gradle build', (err, stdout, stderr) => {
-        if (err) {
-          console.error(err);
-          return;
-        }
-        console.log(stdout);
-      });
+
+
+// Set the path to the Android app source code directory
+const appDir = join(__dirname, 'gaming_app_apk_v103');
+
+
+
+const command = `cd ${appDir} && gradlew assembleDebug`;
+
+exec(command, (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Error executing command: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.error(`Command error: ${stderr}`);
+    return;
+  }
+  console.log(`Command output: ${stdout}`);
+  
+  // APK file generated, do something with it here
+});
+
   
 };
