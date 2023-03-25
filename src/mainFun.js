5b52b440f5503f729fbe8959e0d657338dad76c4
Adarsh Hatkar <80952515+AdarshHatkar@users.noreply.github.com>
2023-03-25 08:02:11 +0530

    rename completed and copied to out put folder

--- src/mainFun.js
+++ src/mainFun.js
@@ -2,41 +2,60 @@ import require from "./require.js";
 import { exec } from "node:child_process";
 import process from "node:process";
 import { fileURLToPath } from 'url';
-import { dirname ,join} from 'path';
+import { dirname, join } from 'path';
+import { copyFile, fsync, rename } from "node:fs";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 // gradle --version
-export let mainFun = () => {
-    console.log("hello world");
-   
- 
+export let mainFun = async() => {
+
+
+  console.log("Script Started-------------------");
+
+  let packageName = 'com.gaming_apk.web_apk';
+  let userName = 'gaming_apk';
+  let version = 10300;
 
 
 
-// Set the path to the Android app source code directory
-const appDir = join(__dirname, '../gaming_app_apk_v103');
+  // android source code location
+  const appSourceCodeDir = join(__dirname, '../gaming_app_apk_v103');
+  const debugApkDir = join(__dirname, '../gaming_app_apk_v103/app/build/outputs/apk/debug');
+  const outputApksDir = join(__dirname, '../outputApks');
+  //console.log(appSourceCodeDir);
+
+  const command = `cd ${appSourceCodeDir} && gradlew assembleDebug`;
+
+  exec(command, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error executing command: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.error(`Command error: ${stderr}`);
+      return;
+    }
+    console.log(`Command output: ${stdout}`);
+
+    // APK file generated, do something with it here
+    const debugApk = join(debugApkDir, 'app-debug.apk');
+    const renamedApk = join(debugApkDir, `${userName}V${version}.apk`);
+    const outputApk = join(outputApksDir, `${userName}V${version}.apk`);
+    rename(debugApk, renamedApk,(err)=>{
+      if (err) throw err;
+      console.log('Rename complete!');
+
+      copyFile(renamedApk, outputApk, (err) => {
+        if (err) throw err;
+        console.log('renamedApk was copied to output folder');
+
+        console.log("apk generated");
+      });
+    })
+    
+  });
 
 
-
-const command = `cd ${appDir} && gradlew assembleDebug`;
-
-exec(command, (error, stdout, stderr) => {
-  if (error) {
-    console.error(`Error executing command: ${error.message}`);
-    return;
-  }
-  if (stderr) {
-    console.error(`Command error: ${stderr}`);
-    return;
-  }
-  console.log(`Command output: ${stdout}`);
-  
-  // APK file generated, do something with it here
-
-  console.log("apk generated");
-});
-
-  
 };
